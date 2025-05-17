a3df50ed61d59eaf24404b67717d48a400a822fd
Saul <123047731@upq.edu.mx>
2025-05-16 18:37:19 -0600

    Booleanos, comparación y lógicos
    Se añadieron los operadores booleanos, para comparar y loogicos

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -3,34 +3,57 @@
 
 # <- con ese símbolo se hacen comentarios
 
-"""<- con las tres comillas se pueden
-COMENTAR varias líneas"""
+#"""<- con las tres comillas se pueden
+#COMENTAR varias líneas"""
 #Un commit es un cambio registrado
 
 # 2. STRINGS
-print("Hola, soy una cadena")
-print('yo soy otra')
+#print("Hola, soy una cadena")
+#print('yo soy otra')
 
-variable1= "hola soy una cadena"
-print(len(variable1)) #LEN sirve para contar los carácteres junto con los espacios
-print(variable1[2:5])
-print(variable1[2:])
-print(variable1[:5]) # : Es un hasta, un delimitador
+#variable1= "hola soy una cadena"
+#print(len(variable1)) #LEN sirve para contar los carácteres junto con los espacios
+#print(variable1[2:5])
+#print(variable1[2:])
+#print(variable1[:5]) # : Es un hasta, un delimitador
 
 # 3. variables
-jdjajfajfea = 0 #No se puede declarar una variable con un número al inicio
+#jdjajfajfea = 0 #No se puede declarar una variable con un número al inicio
 
-x= "Saul Silva Ongay"
-x= 4
-x= 5
+#x= "Saul Silva Ongay"
+#x= 4
+#x= 5
 
-print(x)
+#print(x)
 
-x= int(3)
-y= float(3)
-z= str(3)
+#x= int(3)
+#y= float(3)
+#z= str(3)
 
-print(x,y,z)
-print(type(x))
-print(type(y))
-print(type(z)) #<- La función type muestra el tipo de valor que tiene una variable
+#print(x,y,z)
+#print(type(x))
+#print(type(y))
+#print(type(z)) #<- La función type muestra el tipo de valor que tiene una variable
+
+# 3. Solicitud de datos
+
+#a= input ("introduce cualquier dato: ")
+
+#b= int(input("Introduce un número entero: "))
+#c= float(input("Introduce un número decimal: "))
+       
+#print(a,b,c)
+
+#4. Booleanos, comparacion y logicos
+
+print(10 > 9)
+print(10 < 9)
+print(10 == 9)
+print(10 >= 9)
+print(10 <= 9)
+print(10 != 9)
+
+x= 1 
+print(x<5 and x<10)
+print(x<5 or x<10)
+print((x<5 and x<10))
