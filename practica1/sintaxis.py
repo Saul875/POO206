83dbe71914bd00b1fb3741e09c421edf2ca05dd4
Saul <123047731@upq.edu.mx>
2025-05-16 18:19:12 -0600

    Variables

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -10,3 +10,27 @@ COMENTAR varias líneas"""
 # 2. STRINGS
 print("Hola, soy una cadena")
 print('yo soy otra')
+
+variable1= "hola soy una cadena"
+print(len(variable1)) #LEN sirve para contar los carácteres junto con los espacios
+print(variable1[2:5])
+print(variable1[2:])
+print(variable1[:5]) # : Es un hasta, un delimitador
+
+# 3. variables
+jdjajfajfea = 0 #No se puede declarar una variable con un número al inicio
+
+x= "Saul Silva Ongay"
+x= 4
+x= 5
+
+print(x)
+
+x= int(3)
+y= float(3)
+z= str(3)
+
+print(x,y,z)
+print(type(x))
+print(type(y))
+print(type(z)) #<- La función type muestra el tipo de valor que tiene una variable
